9ddc69986eeb01ecacf56497434a579d02476ec7
CUI Bo (externe) <BCUI-ext@mgen.fr>
2022-03-30 17:11:55 +0200

    test

--- src/main/java/com/bo/springbootkafkaconsumer/config/KafkaProducerConfig.java
+++ src/main/java/com/bo/springbootkafkaconsumer/config/KafkaProducerConfig.java
@@ -2,10 +2,12 @@ package com.bo.springbootkafkaconsumer.config;
 
 import com.bo.springbootkafkaconsumer.models.MyModel;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -20,12 +22,38 @@ public class KafkaProducerConfig {
     @Value("${kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value(value = "${spring.kafka.properties.security_protocol}")
+    private String protocol;
+
+    @Value(value = "${spring.kafka.properties.password_generic}")
+    private String password;
+
+    @Value(value = "${spring.kafka.properties.truststore_and_keystore_location}")
+    private String jksPath;
+
     @Bean
     public ProducerFactory<String, MyModel> myProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+
+
+        configProps.put("security.protocol","SSL");
+        configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, jksPath);
+        configProps.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PKCS12");
+        configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, password);
+
+
+//        if (protocol.equalsIgnoreCase("SSL")) {
+//            configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, jksPath);
+////            configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, password);
+//            configProps.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, jksPath);
+////            configProps.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, password);
+////            configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, password);
+//            configProps.put(SslConfigs.SSL_ENABLED_PROTOCOLS_CONFIG, SslConfigs.DEFAULT_SSL_ENABLED_PROTOCOLS);
+//        }
+
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
